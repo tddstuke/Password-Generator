19633fd94d0ca3b46d56ee7dde3f537443becc31
Todd Stuke <tddstuke@gmail.com>
2022-04-01 21:32:06 -0500

    created password length and confirm character functions

--- script.js
+++ script.js
@@ -1,35 +1,4 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-// ask user to pick a number between 8 and 128 to use as password length
-var getpasswordLength = function () {
-  var passwordLength = prompt("pick a password length between 8 and 128");
-  console.log("length is " + passwordLength);
-  if (passwordLength > 7 && passwordLength < 129) {
-    return true;
-  } else {
-    window.alert("invalid entry. Please try again");
-    getpasswordLength();
-  }
-};
-getpasswordLength();
-
-// ask user to confirm using lowercase, uppercase,  numeric and/or special characters
-
-function generatePassword() {}
-
 // write arrays for lowercase, upper case, numbers, and special characters
-
 var lowerCase = [
   "a",
   "b",
@@ -110,7 +79,70 @@ var specialCharacters = [
   "[",
   "]",
   "^",
-  "{",
   "}",
   "~",
 ];
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// ask user to pick a number between 8 and 128 to use as password length
+var getpasswordLength = function () {
+  var passwordLength = prompt("pick a password length between 8 and 128");
+  console.log("length is " + passwordLength);
+  if (passwordLength > 7 && passwordLength < 129) {
+    return passwordLength;
+  } else {
+    window.alert("invalid entry. Please try again");
+    getpasswordLength();
+  }
+};
+
+// ask user to confirm using lowercase, uppercase,  numeric and/or special characters
+var getLowerCharacters = function () {
+  var getLowerCase = window.confirm("Would you like to use lowercase letters?");
+  if (getLowerCase) {
+    console.log(lowerCase);
+    return lowerCase;
+  }
+};
+var getUpperCharacters = function () {
+  var getUpperCase = window.confirm("Would you like to use uppercase letters?");
+  if (getUpperCase) {
+    console.log(upperCase);
+    return upperCase;
+  }
+};
+var getNumbers = function () {
+  var getNumbers = window.confirm("Would you like to use numbers?");
+  if (getNumbers) {
+    console.log(numbers);
+    return numbers;
+  }
+};
+var getSpecials = function () {
+  var getSpecials = window.confirm("Would you like to use Special Characters?");
+  if (getSpecials) {
+    console.log(specialCharacters);
+    return specialCharacters;
+  }
+};
+
+function generatePassword() {
+  getpasswordLength();
+  getLowerCharacters();
+  getUpperCharacters();
+  getNumbers();
+  getSpecials();
+}
